b40451cc13333ece0bc6619cdbfe6f13e9f6425d
sufyanAbbasi <suabbasi@vassar.edu>
2015-07-08 16:06:13 -0400

    Bounds to BounSpecification
    C’mon Chrome.

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     id: 'main',
-    bounds: { width: 1000, height: 800},
+    innerBounds: { width: 700, height: 555 },
     "resizable": false,
   });
 });
